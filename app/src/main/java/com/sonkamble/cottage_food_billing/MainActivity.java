f71e42273e71de3bec77de576098d53c4f5bbbf7
Divya Bhavar <divyabhavar735@gmail.com>
2025-05-07 11:06:21 +0530

    Add files via upload

---
+++ app/src/main/java/com/sonkamble/cottage_food_billing/MainActivity.java
@@ -0,0 +1,14 @@
+package com.sonkamble.cottage_food_billing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
